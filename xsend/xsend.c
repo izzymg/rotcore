aaee2d1782a5a9898bda5f7dabbc58fe83c7d9ab
izzymg <izzymhg@gmail.com>
2020-02-20 17:23:45 +1300

    XSend -> play pipeline from argument

--- xsend/xsend.c
+++ xsend/xsend.c
@@ -1,3 +1,27 @@
-int main() {
+#include <gst/gst.h>
+
+// XSend: run a gstreamer pipeline. Izzymg.
+
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        g_error("Expected 1 argument: pipeline");
+        return EXIT_FAILURE;
+    }
+    gst_init(&argc, &argv);
+    GError *err = NULL;
+    GstElement *pipeline = gst_parse_launch(argv[1], &err);
+    if(err != NULL) {
+        g_error(err->message);
+        g_error_free(err);
+        return EXIT_FAILURE;
+    }
+
+    int ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    g_assert(ret == 1);
+
+    g_message("Pipeline playing\n");
+
+    gst_deinit();
+    gst_object_unref(pipeline);
     return 0;
 }
