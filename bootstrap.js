2887387eb72a4a1ed00a5dd046320edcc438b4f7
izzymg <izzymhg@gmail.com>
2020-03-05 17:45:04 +1300

    Use `console.log` across bootstrap

--- bootstrap.js
+++ bootstrap.js
@@ -9,26 +9,11 @@ const path =  require("path");
 const secretPath = path.join(__dirname, "secret.txt");
 
 /**
- * Simple console log wrapper.
- * @param {boolean} err Is this an error log, or stdout.
- * @param  {...any} data Data to be logged.
-*/
-const log = (err, ...data) => {
-    let write = console.log;
-    if(err) {
-        write = console.error;
-    }
-    write(new Date(Date.now()).toLocaleString(), ...data);
-};
-
-
-/**
  * Read the GStreamer pipeline into memory.
 */
 const readPipeline = () => {
-    log(false, "Reading pipeline...");
+    console.log("Reading pipeline...");
     const pipeline = readFileSync("conf/send.pipeline", { encoding: "utf8" });
-    log(false, pipeline);
     return pipeline;
 };
 
@@ -62,7 +47,7 @@ let processes = [
 let running = [];
 
 const exit = signal => {
-    log(false, `Received ${signal}, cleaning up children`);
+    console.log(`Received ${signal}, cleaning up children`);
     running.forEach(child => child.kill(signal));
 };
 
@@ -71,23 +56,23 @@ process.on("SIGTERM", exit);
 process.on("beforeExit", exit);
 
 (async () => {
-    log(false, "Bootstrap spawning children");
+    console.log("Bootstrap spawning children");
     for(const p of processes) {
 
-        log(false, `Starting ${p.program}`);
+        console.log(`Starting ${p.program}`);
 
         // Mix process.env with env configuration
         const child = spawn(p.program, p.args, { stdio: 'inherit', env: { ...process.env, ...p.env, } });
 
         child.on("error", err => {
-            log(true, `${p.program} exited with err: ${err}`);
+            console.log(`${p.program} exited with err: ${err}`);
             exit("SIGKILL");
             return;
         });
 
         // Hook events
         child.on("exit", code => {
-            log(true, `${p.program} exited with code: ${code}`);
+            console.log(`${p.program} exited with code: ${code}`);
             exit("SIGKILL");
             return;
         });
