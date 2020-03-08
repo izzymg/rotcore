02c63567060429d0bd351b84512823a00643df4b
izzymg <izzymhg@gmail.com>
2020-03-08 15:03:38 +1300

    Deploy: mixin the process env to spawns
    This change allows for environment variables set outside of deploy.js to be passed into children when they're spawned. Crucial to be able to run browsers, GTK apps, etc.

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -16,13 +16,19 @@ const { spawn } = require("child_process");
 const secretFilePath = pathJoin(__dirname, "secret.txt");
 
 /**
- * Constructs a new process object.
+    Constructs a new process object.
+    Processes have their environment setting mixed in with `process.env`,
+    to allow for external envs to affect the program. Otherwise important things like
+    XDG_RUNTIME_DIR and so on will not be passed down to children.
+
+    The processes are started in "directory" so they may use relative paths.
 */
 const Process = function({ directory, executable, args, environment, }) {
+
     this.directory = directory || "./";
     this.exe = pathJoin(__dirname, this.directory, executable || "noop");
     this.args = args || [];
-    this.env = environment || {  };
+    this.env = { ...process.env, ...(environment || {  }) };
 
     let child;
 
@@ -37,7 +43,7 @@ const Process = function({ directory, executable, args, environment, }) {
 
     /** Spawn the process in the given work-dir. */
     this.start = () => {
-        console.log(`Starting ${this.exe} ${this.args}`);
+        console.log(`Starting ${this.exe} ${this.args.join(" ")}`);
         child = spawn(this.exe, this.args, {
             stdio: "inherit",
             cwd: this.directory,
@@ -47,8 +53,6 @@ const Process = function({ directory, executable, args, environment, }) {
 };
 
 const init = function() {
-
-
     const x11 = new Process({
         directory: "smallbrowse",
         executable: "display.sh",
@@ -64,13 +68,13 @@ const init = function() {
     const stream = new Process({
         directory: ".",
         executable: "stream.sh",
-        environment: { display: config.display, }
+        environment: { DISPLAY: config.display, }
     });
 
     const kbm = new Process({
         directory: "bin/kbm/release",
         executable: "kbm",
-        environment: { display: config.display, },
+        environment: { DISPLAY: config.display, },
         args: [config.rtcAddress, secretFilePath],
     });
 
